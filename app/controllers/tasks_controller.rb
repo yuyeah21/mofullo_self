b4c495a90a2519def485b5fecf7b4a014d5b2ab6
“yuyeah21” <yuyeah21@gmail.com>
2019-05-11 06:47:32 -0500

    削除機能を作成しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,11 @@ class TasksController < ApplicationController
     @task.update(task_params)
     redirect_to(@task)
   end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 private
   def task_params
     params.require(:task).permit(:title, :body, :due_date)
